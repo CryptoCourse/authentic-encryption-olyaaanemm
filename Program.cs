e6d9476f9ee5d8c62143bf4398e7ca0ebdc35e7e
olyaaanemm <90577981+olyaaanemm@users.noreply.github.com>
2022-05-03 19:25:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Text;
 
@@ -8,7 +8,7 @@ namespace Dotnet.AuthentificationMode
     {
         static void Main(string[] args)
         {
-            var mb100 = 1000;
+            var mb100 = 104857600;
             var key = Salt.CreateSalt(16);
             var macKey = Salt.CreateSalt(16);
             var iv = Salt.CreateSalt(16);
@@ -19,15 +19,16 @@ namespace Dotnet.AuthentificationMode
             Console.WriteLine("IV : " + BitConverter.ToString(iv));
             Console.WriteLine("Key : " + BitConverter.ToString(key));
             Console.WriteLine("MAC Key : " + BitConverter.ToString(macKey));
+            Console.WriteLine("plain text : " + BitConverter.ToString(plainText));
             
-            string path1 = @"/Users/olganemova/RiderProjects/Dotnet.AuthentificationMode/Dotnet.AuthentificationMode/plain.txt";
-            if (!File.Exists(path1))
-            {
-                using (StreamWriter sw = File.CreateText(path1))
-                {
-                    sw.WriteLine(BitConverter.ToString(plainText));
-                }	
-            }
+            //string path1 = @"/Users/olganemova/RiderProjects/Dotnet.AuthentificationMode/Dotnet.AuthentificationMode/plain.txt";
+            //if (!File.Exists(path1))
+            //{
+            //    using (StreamWriter sw = File.CreateText(path1))
+            //    {
+            //        sw.WriteLine(BitConverter.ToString(plainText));
+            //    }	
+            //}
 
             using (AuthenticEncryptor encryptor = new AuthenticEncryptor(Mode.Encryption))
             {
@@ -36,15 +37,15 @@ namespace Dotnet.AuthentificationMode
                 encryptor.IV = iv;
                 cipherText = encryptor.ProcessData(plainText);
                 
-                string path2 = @"/Users/olganemova/RiderProjects/Dotnet.AuthentificationMode/Dotnet.AuthentificationMode/cipher.txt";
-                if (!File.Exists(path2))
-                {
-                    using (StreamWriter sw = File.CreateText(path2))
-                    {
-                        sw.WriteLine(BitConverter.ToString(cipherText));
-                    }	
-                }
-                //Console.WriteLine("cipher text : " + BitConverter.ToString(cipherText));
+                //string path2 = @"/Users/olganemova/RiderProjects/Dotnet.AuthentificationMode/Dotnet.AuthentificationMode/cipher.txt";
+                //if (!File.Exists(path2))
+                //{
+                //    using (StreamWriter sw = File.CreateText(path2))
+                //    {
+                //        sw.WriteLine(BitConverter.ToString(cipherText));
+                //    }	
+                //}
+                Console.WriteLine("cipher text : " + BitConverter.ToString(cipherText));
             }
             
             using (AuthenticEncryptor decryptor = new AuthenticEncryptor(Mode.Decryption))
@@ -54,15 +55,15 @@ namespace Dotnet.AuthentificationMode
                 decryptor.IV = iv;
                 possibleText = decryptor.ProcessData(cipherText);
                 
-                string path3 = @"/Users/olganemova/RiderProjects/Dotnet.AuthentificationMode/Dotnet.AuthentificationMode/decrypted.txt";
-                if (!File.Exists(path3))
-                {
-                    using (StreamWriter sw = File.CreateText(path3))
-                    {
-                        sw.WriteLine(BitConverter.ToString(possibleText));
-                    }	
-                }
-                //Console.WriteLine("decrypted text : " + BitConverter.ToString(possibleText));
+                //string path3 = @"/Users/olganemova/RiderProjects/Dotnet.AuthentificationMode/Dotnet.AuthentificationMode/decrypted.txt";
+                //if (!File.Exists(path3))
+                //{
+                //    using (StreamWriter sw = File.CreateText(path3))
+                //    {
+                //        sw.WriteLine(BitConverter.ToString(possibleText));
+                //    }	
+                //}
+                Console.WriteLine("decrypted text : " + BitConverter.ToString(possibleText));
             }
         }
     }
