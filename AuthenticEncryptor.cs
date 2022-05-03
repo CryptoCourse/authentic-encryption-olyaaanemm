c15685a76e744a2eacee3948a4a29f72a4493645
olyaaanemm <90577981+olyaaanemm@users.noreply.github.com>
2022-05-03 19:25:05 +0300

    Update AuthenticEncryptor.cs

--- AuthenticEncryptor.cs
+++ AuthenticEncryptor.cs
@@ -3,17 +3,17 @@ using System.Security.Cryptography;
 
 namespace Dotnet.AuthentificationMode
 {
-    public class AuthenticEncryptor : IDisposable // AES-CTR and HMAC-SHA-256
+    public class AuthenticEncryptor : IDisposable   // AES-CTR and HMAC-SHA-256
     {
         private const int MAC_SIZE = 32;
         private const int DATA_BLOCK_SIZE = 16;
         
         private readonly Mode _mode;
         
-        private int offset;
+        private int _offset;
         public byte[] IV { get; set; }
-        private byte[] Mac { get; set; }
         private byte[] Buffer { get; set; }
+        private byte[] Mac { get; set; }
         public  byte[] Key { get; set; }
         public byte[] MacKey { get; set; }
         private ICryptoTransform Aes128Encryptor { get; set; }
@@ -31,16 +31,13 @@ namespace Dotnet.AuthentificationMode
             _ => throw new NotImplementedException($"The {_mode} mode wasn't implemented")
         };
         
-        private byte[] Encrypt(byte[] data) //CTR(IV) || ED = AES(data) || MAC( CTR(IV) || ED)
+        private byte[] Encrypt(byte[] data)
         {
-            InicializeEncryptor();
+            Initialize();
             
-            //Console.WriteLine(BitConverter.ToString(data));
-            //Console.WriteLine(BitConverter.ToString(IV));
-           
             int iterations = data.Length / DATA_BLOCK_SIZE;
 
-            for (int i = 1; i < iterations; ++i)
+            for (int i = 1; i <= iterations; ++i)
             {
                 AddBlock(data[((i-1) * DATA_BLOCK_SIZE)..(i * DATA_BLOCK_SIZE)], false);
             }
@@ -54,30 +51,19 @@ namespace Dotnet.AuthentificationMode
             {
                 AddBlock(data[(DATA_BLOCK_SIZE * (iterations-1))..], true);
             }
-            //Console.WriteLine(BitConverter.ToString(Buffer));
-            Mac = HMacSha256.ComputeHash(LogicHelper.Сoncatenate(IV, Buffer));
-            return LogicHelper.Сoncatenate(LogicHelper.Сoncatenate(IV, Buffer), Mac);
+
+            return LogicHelper.Сoncatenate(LogicHelper.Сoncatenate(IV, Buffer), HMacSha256.Hash);
         }
         
         private byte[] Decrypt(byte[] data)
         {
             IV = data[..DATA_BLOCK_SIZE];
             
-            InicializeDecryptor();
+            Initialize();
             
             var insideData = data[DATA_BLOCK_SIZE..(data.Length - MAC_SIZE)];
             var possibleMac = data[^MAC_SIZE..];
-            
-            Mac = HMacSha256.ComputeHash(LogicHelper.Сoncatenate(IV, insideData));
-            
-            if (!LogicHelper.IsEqual(possibleMac, Mac))
-            {
-                Console.WriteLine("Mac and cipher mac aren't equal! The message was transformed...");
-            }
-            
-            //Console.WriteLine(BitConverter.ToString(insideData));
-            //Console.WriteLine(BitConverter.ToString(IV));
-            
+
             int iterations = insideData.Length / DATA_BLOCK_SIZE;
 
             for (int i = 1; i < iterations; ++i)
@@ -86,11 +72,12 @@ namespace Dotnet.AuthentificationMode
             }
 
             AddBlock(insideData[(DATA_BLOCK_SIZE * (iterations-1))..], true);
-            
-            //Console.WriteLine(BitConverter.ToString(possibleMac));
-            //Console.WriteLine(BitConverter.ToString(Mac));
-            //Console.WriteLine(BitConverter.ToString(Buffer));
 
+            if (!LogicHelper.IsEqual(possibleMac,  HMacSha256.Hash))
+            {
+                Console.WriteLine("Mac and cipher mac aren't equal! The message was transformed...");
+            }
+            
             return Buffer;
         }
 
@@ -98,59 +85,63 @@ namespace Dotnet.AuthentificationMode
         {
             if (isFinal)
             {
-                Buffer = LogicHelper.Сoncatenate(
-                    Buffer?[..offset],
-                    Aes128Encryptor.TransformFinalBlock(dataBlock, 0, dataBlock.Length));
-                //Console.WriteLine("1");
-                //Console.WriteLine(BitConverter.ToString(IV));
-                //Console.WriteLine(BitConverter.ToString(Buffer));
-                //Console.WriteLine("Mac");
+                switch (_mode)
+                {
+                    case Mode.Decryption:
+                        HMacSha256.TransformFinalBlock(dataBlock, 0, dataBlock.Length);
+                        Mac = HMacSha256.Hash;
+                        dataBlock = Aes128Encryptor.TransformFinalBlock(dataBlock, 0, dataBlock.Length);
+                        break;
+                    case Mode.Encryption: 
+                        dataBlock = Aes128Encryptor.TransformFinalBlock(dataBlock, 0, dataBlock.Length);
+                        HMacSha256.TransformFinalBlock(dataBlock,0,  dataBlock.Length);
+                        Mac = HMacSha256.Hash;
+                        break;
+                }
             }
             else
             {
-                Buffer = LogicHelper.Сoncatenate(Buffer, dataBlock);
-                offset += Aes128Encryptor.TransformBlock(
-                    Buffer, offset, dataBlock.Length, Buffer, offset);
+                switch (_mode)
+                {
+                    case Mode.Decryption: 
+                        _offset += HMacSha256.TransformBlock(
+                            dataBlock, 0, dataBlock.Length, Mac, _offset);
+                        Aes128Encryptor.TransformBlock(
+                            dataBlock, 0, dataBlock.Length, dataBlock, 0);
+                        break;
+                    case Mode.Encryption: 
+                        Aes128Encryptor.TransformBlock(
+                            dataBlock, 0, dataBlock.Length, dataBlock, 0);
+                        _offset += HMacSha256.TransformBlock(
+                            dataBlock, 0, dataBlock.Length, Mac, _offset);
+                        break;
+                }
             }
+            Buffer = LogicHelper.Сoncatenate(Buffer, dataBlock);
         }
+        private void Initialize()
+        {
+            _offset = 0; 
+            
+            Buffer = null;
+            Mac = null;
+            
+            using (Aes aes128 = Aes.Create())
+            {
+                aes128.Key = Key;
+                aes128.IV = IV;
+                aes128.Padding = PaddingMode.None;
+                aes128.Mode = CipherMode.CFB;
 
-        private void InicializeEncryptor()
-        {
-            offset = 0; 
-            
-            Buffer = null;
-            Mac = null;
-            
-            using (Aes aes128 = Aes.Create())
-            {
-                aes128.Key = Key;
-                aes128.IV = IV;
-                aes128.Padding = PaddingMode.None;
-                aes128.Mode = CipherMode.CFB;
-            
-                Aes128Encryptor = aes128.CreateEncryptor();
-            }
-            
-            HMacSha256 ??= new HMACSHA256();
-            HMacSha256.Key = MacKey;
-            HMacSha256.Initialize();
-        }
-        
-        private void InicializeDecryptor()
-        {
-            offset = 0; 
-            
-            Buffer = null;
-            Mac = null;
-            
-            using (Aes aes128 = Aes.Create())
-            {
-                aes128.Key = Key;
-                aes128.IV = IV;
-                aes128.Padding = PaddingMode.None;
-                aes128.Mode = CipherMode.CFB;
-            
-                Aes128Encryptor = aes128.CreateDecryptor();
+                switch (_mode)
+                {
+                    case Mode.Decryption:
+                        Aes128Encryptor = aes128.CreateDecryptor();
+                        break;
+                    case Mode.Encryption:
+                        Aes128Encryptor = aes128.CreateEncryptor();
+                        break;
+                }
             }
 
             HMacSha256 ??= new HMACSHA256();
